60905c31c5f1df41526487d0f36d1572ceec465e
Morgan Helton <morgan@flox.dev>
2025-10-20 13:43:45 -0500

    fix(go): only do NEF build

---
+++ quotes-app-go/.flox/pkgs/quotes-app-go/default.nix
@@ -0,0 +1,13 @@
+{ buildGoModule }:
+
+let
+  src = ../../../.;
+in
+buildGoModule {
+  pname = "quotes-app-go";
+  version = "0.1.0";
+
+  src = src;
+  # This must be updated when `go.mod` or `go.sum` are changed.
+  vendorHash = "sha256-AgRpj3B9fWoiTcZ5YO2gbW+NXiGHGA6HelMF0YpJ3WE=";
+}
