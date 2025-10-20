1615ab94a28b4089b892f1f5b82fe453c5971861
Morgan Helton <morgan@flox.dev>
2025-10-20 11:55:50 -0500

    feat(go): use redis for storage

--- quotes-app-go/main.go
+++ quotes-app-go/main.go
@@ -1,34 +1,59 @@
 package main
 
 import (
+	"context"
 	_ "embed"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/redis/go-redis/v9"
 )
 
 //go:embed quotes.json
 var quotesFile []byte
 
-var quotes []interface{}
-
-func loadQuotes() {
+func (q QuotesRedis) loadQuotes(ctx context.Context) error {
+	var quotes []string
 	if err := json.Unmarshal(quotesFile, &quotes); err != nil {
-		log.Fatalf("Failed to parse embedded quotes.json: %v", err)
+		return err
 	}
-	fmt.Println("Loaded embedded quotes.json")
+
+	for _, v := range quotes {
+		err := q.writeQuoteToRedis(ctx, v)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func getAllQuotes(w http.ResponseWriter, r *http.Request) {
+func (q QuotesRedis) getAllQuotes(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	quotes, err := q.rdb.SMembers(ctx, q.key).Result()
+	if err != nil {
+		http.Error(w, `{"error": "failed to fetch quotes"}`, http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(quotes)
 }
 
-func getQuoteByIndex(w http.ResponseWriter, r *http.Request) {
+func (q QuotesRedis) getQuoteByIndex(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	quotes, err := q.rdb.SMembers(ctx, q.key).Result()
+	if err != nil {
+		http.Error(w, `{"error": "failed to fetch quote"}`, http.StatusInternalServerError)
+		return
+	}
+
 	vars := mux.Vars(r)
 	indexStr, ok := vars["index"]
 	if !ok {
@@ -43,15 +68,50 @@ func getQuoteByIndex(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(quotes[index])
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintln(w, quotes[index])
+}
+
+type QuotesRedis struct {
+	rdb *redis.Client
+	key string
+}
+
+func NewQuotes(host string) *QuotesRedis {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     host,
+		Password: "",
+		DB:       0,
+	})
+	return &QuotesRedis{rdb: rdb, key: "quotes"}
+}
+
+func (q QuotesRedis) writeQuoteToRedis(ctx context.Context, value string) error {
+	_, err := q.rdb.SAdd(ctx, q.key, value).Result()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
-	loadQuotes()
+	var redisHost string
+	flag.StringVar(&redisHost, "r", "localhost:16379", "hostname for redis")
+	flag.Parse()
+
+	client := NewQuotes(redisHost)
+
+	ctx := context.Background()
+
+	err := client.loadQuotes(ctx)
+	if err != nil {
+		log.Fatalf("Failed to load quotes: %v\n", err)
+	}
 
 	r := mux.NewRouter()
-	r.HandleFunc("/quotes", getAllQuotes).Methods("GET")
-	r.HandleFunc("/quotes/{index}", getQuoteByIndex).Methods("GET")
+	r.HandleFunc("/quotes", client.getAllQuotes).Methods("GET")
+	r.HandleFunc("/quotes/{index}", client.getQuoteByIndex).Methods("GET")
 
 	addr := ":3000"
 	fmt.Printf("Server running on %s\n", addr)
